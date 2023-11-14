3fcdc89612439922eeed09324500264d90b899d2
Alfech <la-fleche.chevrette.1@ens.etsmtl.ca>
2023-11-13 22:25:02 -0500

    Test window + update of bat

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 int main() {
-    std::cout << "Hello, word!" << std::endl;
+    
+    std::cout << "Hello, World!"  << std::endl;
+
+    sf::Window window;
+    window.create(sf::VideoMode(800, 600), "My Window");
+
+    while(window.isOpen()){
+
+        sf::Event event;
+
+        while(window.pollEvent(event)){
+
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
